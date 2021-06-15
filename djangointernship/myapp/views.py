ffa132794096d407fc3894161dd38ecdcb8c1e63
ShahManoli <85156249+ShahManoli@users.noreply.github.com>
2021-06-15 19:57:52 +0530

    Add files via upload

---
+++ djangointernship/myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homepageview(request):
+    return render(request,'home.html')
+
+
+def aboutpageview(request):
+    return render(request,'about.html')
+
+def contactpageview(request):
+    return render(request,'contact.html')
+
+def myform(request):
+    return render(request,'myform.html')
+
+def process(request):
+    print("Welcome")
+    print(request.method)
+    print(request.POST)
+    a= str(request.POST['fname'])
+    print(a)
+    b= str(request.POST['mname'])
+    print(b)
+    c= str(request.POST['lname'])
+    print(c)
+    d= str(request.POST['number'])
+    print(d)
+    e= str(request.POST['email'])
+    print(e)
+    return render(request,'ans.html',{'mya':a,'myb':b,'myc':c,'myd':d,'mye':e})
+
